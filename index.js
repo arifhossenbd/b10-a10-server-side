a159fdbdf4cfc3b13e0bbf2dc03c55b8a16b6cf4
Arif Hossen <arifhossengd@gmail.com>
2025-03-05 15:50:57 +0600

    add GET endpoint for retrieving reviews and refactor POST endpoint for creating reviews

--- index.js
+++ index.js
@@ -75,10 +75,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    //Create a new review
     app.post("/review", async(req, res) => {
       const result = await crudOperation("create", "reviewCollection", req.body);
       res.status(result.success ? 200 : 400).json(result);
     });
+    app.get("/reviews", async(req, res) => {
+      const result = await crudOperation("read", "reviewCollection");
+      res.status(result.success ? 200 : 400).json(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
