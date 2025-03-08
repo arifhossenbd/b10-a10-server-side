e3befde0d16afac1b34f0a8f5897733fa7cc6ecb
Arif Hossen <arifhossengd@gmail.com>
2025-03-08 14:48:29 +0600

    refactor index.js for improved readability and consistency; add CRUD operations for user reviews and watchlist

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-require('dotenv').config()
-const cors = require('cors');
-const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,16 +16,21 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const chillGamer = client.db('ChillGamerDB');
+const chillGamer = client.db("ChillGamerDB");
 const getCollection = (collectionName) => {
   const collection = chillGamer.collection(collectionName);
   return collection;
-}
+};
 /** Reusable Crud Operation Function */
-const crudOperation = async (operation, collectionName, data = {}, filter = {}) => {
+const crudOperation = async (
+  operation,
+  collectionName,
+  data = {},
+  filter = {}
+) => {
   try {
     const collection = getCollection(collectionName);
 
@@ -34,44 +39,76 @@ const crudOperation = async (operation, collectionName, data = {}, filter = {}) 
       try {
         filter._id = new ObjectId(filter._id);
       } catch (error) {
-        return { success: false, message: "Invalid ObjectId format", error }
+        return { success: false, message: "Invalid ObjectId format", error };
       }
     }
     let result;
     switch (operation) {
       case "create":
         result = await collection.insertOne(data);
-        return { success: true, message: `Data inserted successfully in the ${collectionName}`, insertedId: result?.insertedId };
+        return {
+          success: true,
+          message: `Data inserted successfully in the ${collectionName}`,
+          insertedId: result?.insertedId,
+        };
       case "read":
         result = await collection.find(filter).toArray();
-        if (!result) return { success: false, message: `This data not found in ${collectionName}` }
-        return { success: true, message: `Data retrieved successfully to the ${collectionName}`, data: result };
+        if (!result || result.length === 0)
+          return {
+            success: false,
+            message: `No data found in ${collectionName}`,
+          };
+        return {
+          success: true,
+          message: `Data retrieved successfully to the ${collectionName}`,
+          data: result,
+        };
       case "readOne":
         result = await collection.findOne(filter);
-        return { success: true, message: `Data retrieved successfully by id to the ${collectionName}`, data: result };
+        return {
+          success: true,
+          message: `Data retrieved successfully by id to the ${collectionName}`,
+          data: result,
+        };
 
       /** Update full document */
       case "update":
         result = await collection.updateOne(filter, { $set: data });
-        if (result?.modifiedCount === 0) return { success: false, message: "Data not updated" }
-        return { success: true, message: `Data updated successfully by id to the ${collectionName}`, modifiedCount: result?.modifiedCount };
+        if (result?.modifiedCount === 0)
+          return { success: false, message: "Data not updated" };
+        return {
+          success: true,
+          message: `Data updated successfully by id to the ${collectionName}`,
+          modifiedCount: result?.modifiedCount,
+        };
 
       /** Update partial */
       case "patch":
         result = await collection.updateOne(filter, { $set: data });
-        if (result?.modifiedCount === 0) return { success: false, message: "Data not updated" }
-        return { success: true, message: `Data patched successfully by id to the ${collectionName}`, modifiedCount: result?.modifiedCount };
+        if (result?.modifiedCount === 0)
+          return { success: false, message: "Data not updated" };
+        return {
+          success: true,
+          message: `Data patched successfully by id to the ${collectionName}`,
+          modifiedCount: result?.modifiedCount,
+        };
       case "delete":
         result = await collection.deleteOne(filter);
-        if (result?.deletedCount === 0) return { success: false, message: "Data not deleted" }
-        return { success: true, message: `Data deleted successfully by id to the ${collectionName}`, modifiedCount: result?.modifiedCount };
-      default: return { success: false, message: "Invalid operation" }
+        if (result?.deletedCount === 0)
+          return { success: false, message: "Data not deleted" };
+        return {
+          success: true,
+          message: `Data deleted successfully by id to the ${collectionName}`,
+          modifiedCount: result?.modifiedCount,
+        };
+      default:
+        return { success: false, message: "Invalid operation" };
     }
   } catch (error) {
     console.error("Database error", error);
     return { success: false, message: "Database operation failed", error };
   }
-}
+};
 
 async function run() {
   try {
@@ -80,48 +117,115 @@ async function run() {
 
     /** Review related CRUD Operation Start */
     //Create a new review
-    app.post("/review", async(req, res) => {
-      const result = await crudOperation("create", "ReviewCollection", req.body);
+    app.post("/review", async (req, res) => {
+      const result = await crudOperation(
+        "create",
+        "ReviewCollection",
+        req.body
+      );
       res.status(result.success ? 200 : 400).json(result);
     });
 
     // Retrieve data by use get
-    app.get("/reviews", async(req, res) => {
+    app.get("/reviews", async (req, res) => {
       const result = await crudOperation("read", "ReviewCollection");
       res.status(result.success ? 200 : 400).json(result);
-    })
+    });
 
     // Get specific data by id
-    app.get("/review/:id", async(req, res) => {
-      const result = await crudOperation("readOne", "ReviewCollection", {_id: req.params.id});
+    app.get("/review/:id", async (req, res) => {
+      const result = await crudOperation("readOne", "ReviewCollection", {
+        _id: req.params.id,
+      });
       res.status(result.success ? 200 : 400).json(result);
-    })
+    });
 
     // Update specific data by id
-    app.put("/review/:id", async(req, res) => {
-      const filter = {_id: new ObjectId(req.params.id)};
-      const result = await crudOperation("update", "ReviewCollection", req.body, filter);
+    app.put("/review/:id", async (req, res) => {
+      const filter = { _id: new ObjectId(req.params.id) };
+      const result = await crudOperation(
+        "update",
+        "ReviewCollection",
+        req.body,
+        filter
+      );
       res.status(result.success ? 200 : 400).json(result);
-    })
+    });
 
-    // Delete specific data by id
-    app.delete("/review/:id", async(req, res) => {
-      const filter = {_id: new ObjectId(req.params.id)};
-      const result = await crudOperation("delete", "ReviewCollection", req.body, filter);
-      res.status(result.success ? 200 : 400).json(result);
-    })
     /** Review related CRUD Operation End */
 
+    /** My Reviews related CRUD Operation Start */
+    app.get("/myReviews/:emailOrName", async (req, res) => {
+      try {
+        const emailOrName = decodeURIComponent(req.params.emailOrName);
+        const filter = {
+          $or: [
+            { userEmail: { $regex: new RegExp(emailOrName, "i") } },
+            { userName: { $regex: new RegExp(emailOrName, "i") } },
+          ],
+        };
+        const result = await crudOperation(
+          "read",
+          "ReviewCollection",
+          {},
+          filter
+        );
+        res.status(result.success ? 200 : 400).json(result);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    // Get specific data by id
+    app.get("/myReview/:id", async (req, res) => {
+      const result = await crudOperation("readOne", "ReviewCollection", {
+        _id: req.params.id,
+      });
+      res.status(result.success ? 200 : 400).json(result);
+    });
+
+    // Delete specific data by id
+    app.delete("/myReview/:id", async (req, res) => {
+      const filter = { _id: new ObjectId(req.params.id) };
+      const result = await crudOperation(
+        "delete",
+        "ReviewCollection",
+        req.body,
+        filter
+      );
+      res.status(result.success ? 200 : 400).json(result);
+    });
+
+    // Update specific data by id
+    app.put("/myReview/:id", async (req, res) => {
+      const filter = { _id: new ObjectId(req.params.id) };
+      const result = await crudOperation(
+        "update",
+        "ReviewCollection",
+        req.body,
+        filter
+      );
+      res.status(result.success ? 200 : 400).json(result);
+    });
+
+    /** My Reviews related CRUD Operation Start */
+
     /** WatchList related CRUD Operation Start */
-    app.post("/watchList", async(req, res) => {
-      const result = await crudOperation("create", "WatchListCollection", req.body);
+    app.post("/watchList", async (req, res) => {
+      const result = await crudOperation(
+        "create",
+        "WatchListCollection",
+        req.body
+      );
       res.status(result.success ? 200 : 400).json(result);
     });
     /** WatchList related CRUD Operation End */
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -129,10 +233,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send("Data is coming on the server...!")
-})
+app.get("/", (req, res) => {
+  res.send("Data is coming on the server...!");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running http://localhost:${port}`)
-})
+  console.log(`Server is running http://localhost:${port}`);
+});
